67849d4c5361aa16b4f6d8e1bef223405c9c6e8c
juandsuarezz <1486@holbertonschool.com>
2021-03-24 22:52:20 -0500

    Update 1.1

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,13 +6,32 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject hambBulletPrefab;
+    public GameObject playerHamburguesa;
+    public GameObject playerPan;
+    public Animator anim;
+
+    void Start()
+    {
+        playerHamburguesa.SetActive(true);
+        playerPan.SetActive(false);
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.Z))
         {
             Shoot();
+            playerHamburguesa.SetActive(true);
+            playerPan.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            playerHamburguesa.SetActive(false);
+            playerPan.SetActive(true);
+            anim.SetTrigger("pan");
         }
     }
 
